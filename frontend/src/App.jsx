daa703ab1985fcc43c13a8aa57643c038f9717f0
= <=>
2026-02-23 21:55:36 +0530

    frontend changes from html,css,javascript to react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Report from './pages/Report.jsx'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/"          element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/report"    element={<Report />} />
+    </Routes>
+  )
+}
